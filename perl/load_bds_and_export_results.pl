3c9565150dcd3d79309e995d9edd8c03e79d5b9f
Shawn Laffan <shawnlaffan@gmail.com>
2015-01-31 12:14:47 +1100

    Change tabs to spaces.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- perl/load_bds_and_export_results.pl
+++ perl/load_bds_and_export_results.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/perl -w
 
-#  example script for reading in a set of trees fromn a nexus file and then
-#  linking them to an existing basedata object
-
 use strict;
 use warnings;
 use 5.010;
@@ -12,9 +9,6 @@ use File::Spec;  #  for the cat_file sub
 use English qw ( -no_match_vars );
 
 use Biodiverse::BaseData;
-#use Biodiverse::ElementProperties;  #  for remaps
-#use Biodiverse::ReadNexus;
-#use Biodiverse::Tree;
 
 
 #  load up the user defined libs
@@ -35,7 +29,7 @@ $| = 1;
 
 my ($opt, $usage) = describe_options(
   '%c <arguments>',
-  [ 'input_bds_file=s',   'The input basedata file .bds', { required => 1 } ],
+  [ 'input_bds_file=s',     'The input basedata file .bds', { required => 1 } ],
   [ 'output_csv_prefix:s',  'The output biodiverse results file prefix (.csv is appended)'],
   [],
   [ 'help',       "print usage message and exit" ],
@@ -54,7 +48,7 @@ my $output_csv_prefix = $opt->output_csv_prefix;
 ###  read in the basedata object
 my $bd;
 eval {
-	$bd = Biodiverse::BaseData->new (file => $input_bds_file);
+    $bd = Biodiverse::BaseData->new (file => $input_bds_file);
 };
 croak $@ if $@;
 
@@ -64,28 +58,28 @@ my @outputs = $bd->get_spatial_output_refs;
 
 
 foreach my $output (@outputs) {
-	my @lists = $output->get_lists_across_elements;
-	foreach my $list (@lists) {
-		say $list;
-		my $list_fname = $list;
-		if ($list_fname =~ s/>>/--/) {  #  CHANGE THIS LINE TO ALTER THE >> REPLACEMENT TEXT IN FILENAME
-			say 'Output name changed - now it is ' . $list_fname;
-		}
-		my $csv_file = sprintf "%s_%s.csv", $output_csv_prefix, $list_fname;
-		$output->export (
-			file   =>  $csv_file,
-			format => 'Delimited text',
-			list   => $list,
-		);
-		
-	}
+    my @lists = $output->get_lists_across_elements;
+    foreach my $list (@lists) {
+        say $list;
+        my $list_fname = $list;
+        if ($list_fname =~ s/>>/--/) {  #  CHANGE THIS LINE TO ALTER THE >> REPLACEMENT TEXT IN FILENAME
+            say 'Output name changed - now it is ' . $list_fname;
+        }
+        my $csv_file = sprintf "%s_%s.csv", $output_csv_prefix, $list_fname;
+        $output->export (
+            file   =>  $csv_file,
+            format => 'Delimited text',
+            list   => $list,
+        );
+        
+    }
 };
 
 my $csv_file = sprintf "%s_%s.csv", $output_csv_prefix, "groups";
-		$bd->get_groups_ref->export (
-			file   =>  $csv_file,
-			format => 'Delimited text',
-			list => 'SUBELEMENTS',
-		);
+$bd->get_groups_ref->export (
+    file   =>  $csv_file,
+    format => 'Delimited text',
+    list => 'SUBELEMENTS',
+);
 say 'Process completed';
 
