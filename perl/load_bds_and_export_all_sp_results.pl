b0346089ef0cae04629dcd4a9711c36da2908f79
Shawn Laffan <shawnlaffan@gmail.com>
2015-01-31 12:30:17 +1100

    Handle multiple basedata files.
    Signed-off-by: Shawn Laffan <shawnlaffan@gmail.com>

--- perl/load_bds_and_export_all_sp_results.pl
+++ perl/load_bds_and_export_all_sp_results.pl
@@ -5,8 +5,9 @@ use warnings;
 use 5.010;
 
 use Carp;  #  warnings and dropouts
-use File::Spec;  #  for the cat_file sub
 use English qw ( -no_match_vars );
+use Path::Class;
+
 
 use Biodiverse::BaseData;
 
@@ -29,7 +30,7 @@ $| = 1;
 
 my ($opt, $usage) = describe_options(
   '%c <arguments>',
-  [ 'input_bds_file|i=s',       'The input basedata file .bds', { required => 1 } ],
+  [ 'input_bds_glob|i=s',       'A glob to select which basedata files are loaded.', { required => 0 } ],
   [ 'output_csv_prefix|opfx:s', 'The output biodiverse results file prefix (.csv is appended)'],
   [],
   [ 'help',       'print usage message and exit' ],
@@ -41,42 +42,48 @@ if ($opt->help) {
     exit;
 }
 
-my $input_bds_file    = $opt->input_bds_file;
+my $input_bds_glob    = $opt->input_bds_glob || '*.bds';
 my $output_csv_prefix = $opt->output_csv_prefix;
 
+my @bd_files = glob $input_bds_glob;
 
-###  read in the basedata object
-my $bd;
-eval {
-    $bd = Biodiverse::BaseData->new (file => $input_bds_file);
-};
-croak $@ if $@;
+foreach my $input_bds_file (@bd_files) {
+    my $bd;
+    eval {
+        $bd = Biodiverse::BaseData->new (file => $input_bds_file);
+    };
+    croak $@ if $@;
+
+    my $file_basename = Path::Class::file($input_bds_file)->basename;
+
+    my @outputs = $bd->get_spatial_output_refs;
+    
+    foreach my $output (@outputs) {
+        my $output_name = $output->get_param('NAME');
+        my @lists = $output->get_lists_across_elements;
+        foreach my $list (@lists) {
+            say $list;
+            my $list_fname = $list;
+            $list_fname =~ s/>>/--/;  #  systems don't like >> in file names
+            my $csv_file = sprintf "%s%s_%s_%s.csv", $output_csv_prefix, $file_basename, $output_name, $list_fname;
+            $output->export (
+                file   =>  $csv_file,
+                format => 'Delimited text',
+                list   => $list,
+            );
+        }
+    };
+
+    #  overwrite these for now as we assume they are the same
+    my $csv_file = sprintf "%s_%s.csv", $output_csv_prefix, "groups";
+    $bd->get_groups_ref->export (
+        file   =>  $csv_file,
+        format => 'Delimited text',
+        list => 'SUBELEMENTS',
+    );
+
+}
 
 
-my @outputs = $bd->get_spatial_output_refs;
-
-foreach my $output (@outputs) {
-    my $output_name = $output->get_param('NAME');
-    my @lists = $output->get_lists_across_elements;
-    foreach my $list (@lists) {
-        say $list;
-        my $list_fname = $list;
-        $list_fname =~ s/>>/--/;  #  systems don't like >> in file names
-        my $csv_file = sprintf "%s_%s_%s.csv", $output_csv_prefix, $output_name, $list_fname;
-        $output->export (
-            file   =>  $csv_file,
-            format => 'Delimited text',
-            list   => $list,
-        );
-        
-    }
-};
-
-my $csv_file = sprintf "%s_%s.csv", $output_csv_prefix, "groups";
-$bd->get_groups_ref->export (
-    file   =>  $csv_file,
-    format => 'Delimited text',
-    list => 'SUBELEMENTS',
-);
 say 'Process completed';
 
