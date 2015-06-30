30f0561d0348e013f36e0091c896033fed00cb6d
Nunzio Knerr <Nunzio.Knerr@csiro.au>
2015-06-30 16:15:42 +1000

    more packages

--- R_release/check_and_install_packages.R
+++ R_release/check_and_install_packages.R
@@ -1,17 +1,18 @@
 ##########################################################################################################
+# RUN THIS SCRIPT 1st
 #
 #This script checks and installs a number of packages if they are not already installed on the system
 #to use it just select all of the text and run it in R. It basically speeds up the process of getting an R install up and running so you don't need to manually install packages.
 #
 #Nunzio.Knerr@csiro.au
-#Date:26/05/2014
+#updated Date:19/06/2015
 #
 ##########################################################################################################
 #
 #below is a list of packages to check and install
 #
 stdpkgs <- c("sp", "maptools", "XML", "gridExtra", "Cairo", "rgdal", "extrafont", "grid",  "rgeos", "raster", "plyr", "dplyr", "tidyr", "ggplot2", "RColorBrewer", "colorspace", "colorRamps", "spacetime", "aqp", "spatstat", "scales", "stringr", "gWidgets", "phytools", "ape", "apTreeshape") 
-otherpkgs <- c("plotKML") 
+otherpkgs <- c("plotKML", "maps", "mapdata") 
 #
 #Do not edit below
 
